1286ec43feb96aa4895b4c0deb39caaa05ce8240
Junya <junya.ishibuchi@gmail.com>
2023-11-08 22:41:37 -0800

    feat: implemented according to trpc docs called set up with Next.js
    see
    https://trpc.io/docs/client/nextjs/setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { trpc } from '../utils/trpc';
+import {AppProps, AppType} from "next/app";
+const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+export default trpc.withTRPC(MyApp);
